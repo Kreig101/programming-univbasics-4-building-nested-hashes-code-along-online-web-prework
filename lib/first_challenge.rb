a39a0906ded4b544b0ca5146d57e869506b443a0
Craig Storer <craigstorer@gmail.com>
2020-02-18 00:08:22 +0000

    Automatically backed up by Learn

--- lib/first_challenge.rb
+++ lib/first_challenge.rb
@@ -1,6 +1,10 @@
 def first_challenge
    epic_tragedy = {
-  :montague => {},
+  :montague => {
+    :patriarch => {
+      :name => "Lord Montague",
+      :age => "53"
+  },
   :capulet => {}
 }
 end
